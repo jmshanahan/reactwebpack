359cdcbc5bad3e44810620c34db01a4f11161448
jmshanahan <comeraghsolutions@gail.com>
2019-06-19 15:50:04 +0100

    polyfill added

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,12 +1,14 @@
+const path = require('path');
 const webpack = require('webpack');
 const merge = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin'); 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const APP_DIR = path.resolve(__dirname, '../src');
 module.exports =  env => {
     const {PLATFORM, VERSION} = env;
     return merge ({
+      entry: ['@babel/polyfill', APP_DIR],
     module: {
       rules: [
         {
